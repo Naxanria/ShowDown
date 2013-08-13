04c826f1f7ec8ac3e7ed2e13662c21085f3d8c04
Naxanria <gerbrandnijk@gmail.com>
2013-08-13 06:39:22 +0200

    Fixed NP at death

--- src/nl/naxanria/showdown/events/PlayerDeath.java
+++ src/nl/naxanria/showdown/events/PlayerDeath.java
@@ -1,5 +1,6 @@
 package nl.naxanria.showdown.events;
 
+import nl.naxanria.showdown.Core;
 import nl.naxanria.showdown.handlers.PlayerHandler;
 import no.runsafe.framework.api.event.player.IPlayerDeathEvent;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerDeathEvent;
@@ -7,9 +8,10 @@ import no.runsafe.framework.minecraft.event.player.RunsafePlayerDeathEvent;
 public class PlayerDeath implements IPlayerDeathEvent
 {
 
-	public PlayerDeath(PlayerHandler playerHandler)
+	public PlayerDeath(PlayerHandler playerHandler, Core core)
 	{
 		this.playerHandler = playerHandler;
+		this.core = core;
 	}
 
 	@Override
@@ -18,9 +20,13 @@ public class PlayerDeath implements IPlayerDeathEvent
 		if (playerHandler.isInGame(event.getEntity()))
 		{
 			playerHandler.removePlayer(event.getEntity());
+			if(playerHandler.getAmountPlayers() == 1)
+				core.winner();
+
 		}
 	}
 
 	private final PlayerHandler playerHandler;
+	private final Core core;
 
 }
